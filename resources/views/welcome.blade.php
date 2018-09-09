68654e2255e6f74189d14f2b454a75da4530c1c1
Marlon Ferreira <ma.ferreira93@gmail.com>
2018-09-09 17:44:48 -0300

    Fix in Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Panthera Systems
+                    {{ env('APP_NAME') }}
                 </div>
 
                 <div class="links">
